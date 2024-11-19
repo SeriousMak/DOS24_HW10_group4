17d6080d16ef85a264658d40398e2145afabcb44
Sergey Selivonchik <elhom99@gmail.com>
2024-11-19 20:35:16 +0300

    Add functional block: List of users

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ user_management.sh <param1-4>
 param1 - adding a user
 param2 - deleting a user 
 param1 - change password
-param1 - list of users
+param4 - list of users
 ```
--- user_management.sh
+++ user_management.sh
@@ -5,6 +5,9 @@ case "\$1" in
   2)  
   3)  
   4)  
+  echo "List of users:"  
+    cut -d: -f1 /etc/passwd  
+    ;;  
   *)  
     echo "Invalid option. Please use 1-4."  
 esac
