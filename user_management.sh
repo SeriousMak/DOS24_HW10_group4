24167780909f0c45b73fb90ffaf6b8effb8cb0c5
Anna Medvedeva <anna@eunify.net>
2024-11-19 19:46:24 +0200

    Create User

--- user_management.sh
+++ user_management.sh
@@ -1,7 +1,12 @@
 #!/bin/bash  
 
 case "\$1" in  
+  case "\$1" in  
   1)  
+    read -p "Enter username to add: " username  
+    sudo useradd "$username"  
+    echo "User $username added."  
+    ;;   
   2)  
   3)  
   4)  
