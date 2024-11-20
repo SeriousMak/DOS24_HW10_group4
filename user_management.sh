cc7667e7c1457b6500c6ea09526eb8454cedc617
SeriousMak <sashamkr@mail.ru>
2024-11-20 18:33:45 +0300

    feature/add change password

--- user_management.sh
+++ user_management.sh
@@ -3,6 +3,10 @@ case "\$1" in
   1)  
   2)  
   3)  
+  read -p "Enter username to change password: " username  
+    sudo passwd "$username"  
+    echo "Password changed for user $username."  
+    ;;  
   4)  
   *)  
     echo "Invalid option. Please use 1-4."  
