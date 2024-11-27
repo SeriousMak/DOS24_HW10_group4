d8c73aaa6fe2b188dfa1d61c5e3e306526c6660f
Denis Moskalenko <shaalis@gmail.com>
2024-11-27 15:23:34 +0100

    Добавил ;; в конце каждой ветви case.

--- user_management.sh
+++ user_management.sh
@@ -1,21 +1,23 @@
-#!/bin/bash  
-case "\$1" in  
-  1)  
-    read -p "Enter username to add: " username  
-    sudo useradd "$username"  
-    echo "User $username added."  
-    ;;   
-  2)  
-  echo "smth" 
-  3)  
-  read -p "Enter username to change password: " username  
-    sudo passwd "$username"  
-    echo "Password changed for user $username."  
-    ;;  
-  4)  
-  echo "List of users:"  
-    cut -d: -f1 /etc/passwd  
-    ;;  
-  *)  
-    echo "Invalid option. Please use 1-4."  
+#!/bin/bash
+case "$1" in
+  1)
+    read -p "Enter username to add: " username
+    sudo useradd "$username"
+    echo "User $username added."
+    ;;
+  2)
+    echo "smth"  # Добавлен ;;
+    ;;
+  3)
+    read -p "Enter username to change password: " username
+    sudo passwd "$username"
+    echo "Password changed for user $username."
+    ;;
+  4)
+    echo "List of users:"
+    cut -d: -f1 /etc/passwd
+    ;;
+  *)
+    echo "Invalid option. Please use 1-4."
+    ;; # Добавлен ;; - важно для правильной работы case
 esac
